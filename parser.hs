5c283f0ae37c218483311df9ce6503163bac29bf
Ben Ruijl <benruyl@gmail.com>
2012-01-31 10:14:34 +0100

    First commit. Started on the data structure.

---
+++ parser.hs
@@ -0,0 +1,8 @@
+-- what to do with Prog = Decl+ ?
+data id = Int -- internal representation of variables
+data RetType = Type | Void
+data Type = Int | Bool | Tuple Type Type | List Type | id
+data Exp = 
+
+main = do putStrLn "test"
+
