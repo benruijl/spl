7a8d9348fdc2904c14a75155cf7641f94f1c5307
Sebastian - Mario Tutuianu - Szigeti <sebitzutzu@yahoo.com>
2012-02-01 11:28:38 +0100

    added VarDecl, FunCall, ActArgs

--- Grammar.hs
+++ Grammar.hs
@@ -5,14 +5,14 @@ type Id = String
 data Prog = Decl
 data Decl = VarDecl | FunDecl
 
--- data VarDecl
--- data FunDecl
+type VarDecl = (Type, (Id, Exp)) -- is it like this or should it be more like ((Type, Id), Exp)?
+-- type FunDecl
 
  -- use tuple of Type and Id as arguments?
  -- bit strange that Type can also be Id
 type FArgs = [(Type, Id)]
--- data ActArgs
--- data FunCall
+type ActArgs = [Exp]
+type FunCall = (Id,[ActArgs])
 
 data Exp = Exp Op2 Exp | Op1 Exp | FunCall | RetType -- is ret type ok in this case otherwise multiple declaration of the same Type?
 data RetType = Type | Void
