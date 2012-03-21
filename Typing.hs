2f0a711ce2a90d80848ffc8ac829ced82be9b41c
Ben Ruijl <benruyl@gmail.com>
2012-03-21 15:05:59 +0100

    Added a class for type checking

--- Typing.hs
+++ Typing.hs
@@ -1,7 +1,9 @@
 import AST
 import Data.List
 
-type Env = (Int, [(Type, Type)]) -- count of generic variables
+-- Environment should keep track of the current Prog and all the function names,
+-- it should also keep track of the function the checker is currently in.
+type Env = (Int, [(Type, Type)]) 
 type TypeChecker =  Env -> (Type, Env)
 
 -- Generate a new type
@@ -35,6 +37,10 @@ class Substitute a where
 	uniqueName :: a -> Env -> ([(Type, Type)], Env)
 	makeUnique :: a -> [(Type, Type)] -> a -- TODO: add env
 
+instance Substitute Decl where
+	substitute a b (VarDecl v) = VarDecl (substitute a b v)
+	substitute a b (FunDecl f) = FunDecl (substitute a b f)
+
 -- BUG: first occurence is not replaced
 instance Substitute FunDecl where
 	substitute a b (FD ret name args vars stmts) = FD (substitute a b ret) name (newargs args) (map (substitute a b) vars) stmts
@@ -60,28 +66,33 @@ instance Substitute Type where
 	typeScraper _ = []
 
 
+fullSubstitute :: Type -> Type -> Prog -> Prog
+fullSubstitute a b c = map (substitute a b) c
 
-		
--- TODO: add support for booleans
-getType1 :: Exp -> TypeChecker
-getType1 (Int _) = yield Int_
-getType1 (Bool _) = yield Bool_
---getType1 (ExpOp_ o a b) = if fst (getType1 a) == fst (getType1 b) && fst (getType1 a) == Int__ then yield Int__ else error $ "Expected type int " ++ "," ++ " int but got " ++ show (fst $ getType1 a) ++ "," ++ show (fst $ getType1 b)
---getType1 (Op1_ o a) = if (fst (getType1 a == Int__) then yield Int__ else error $ "Expected int, but got " ++ show (fst $ getType1 a)
-getType1 EmptyList = freshVar >-> (\x -> List_ x)
-getType1 (Tuple a b) = getType1 a +=+ getType1 b >-> (\(c, d) -> Tuple_ c d)
---getType1 (Id_ v) = envGetType e v
---getType1 (FunCall (f, _)) = getReturnType e f
+class TypeCheck a where
+	-- what should this function return? If it only modifies the environment, it is maybe ok as well.
+	enforce :: a -> Env -> (Type, Env)
 
-getType2 :: Env -> Stmt -> Type
---getType2 e (Assign id1 exp) = if (getType1 e exp) == (getType1 e id1) then (getType1 e id1)
-					--	  else error("Cannot unify types !" ++ show (getType1 e exp) ++ show(getType1 e id1)) so what should we do with id stuff?
-getType2 e (If exp stmt) = if (fst $ getType1 exp e) == Bool_ then Undefined
-						  else error("Cannot unify expected type Bool_ with " ++ show(getType1 exp e) ++"!");
-getType2 e (IfElse exp stmt1 stmt2) = if (fst $ getType1 exp e) == Bool_ then Undefined
-						  else error("Cannot unify expected type Bool_ with " ++ show(getType1 exp e) ++"!");
-getType2 e (While exp stmt) = if (fst $ getType1 exp e) == Bool_ then Undefined
-						  else error("Cannot unify expected type Bool_ with " ++ show(getType1 exp e) ++"!");
-getType2 e (Seq stmt) = Undefined
-getType2 e (FunCall_ funCall) = Undefined
-getType2 e (Return exp) = fst $ getType1 exp e
+instance TypeCheck Exp where	
+	-- TODO: add support for booleans
+	enforce (Int _) = yield Int_
+	enforce (Bool _) = yield Bool_
+	--enforce (ExpOp_ o a b) = if fst (enforce a) == fst (enforce b) && fst (enforce a) == Int__ then yield Int__ else error $ "Expected type int " ++ "," ++ " int but got " ++ show (fst $ enforce a) ++ "," ++ show (fst $ enforce b)
+	--enforce (Op1_ o a) = if (fst (enforce a == Int__) then yield Int__ else error $ "Expected int, but got " ++ show (fst $ enforce a)
+	enforce EmptyList = freshVar >-> (\x -> List_ x)
+	enforce (Tuple a b) = enforce a +=+ enforce b >-> (\(c, d) -> Tuple_ c d)
+	--enforce (Id_ v) = envGetType e v
+	--enforce (FunCall (f, _)) = getReturnType e f
+
+instance TypeCheck Stmt where	
+	--enforce e (Assign id1 exp) = if (enforce e exp) == (enforce e id1) then (enforce e id1)
+						--	  else error("Cannot unify types !" ++ show (enforce e exp) ++ show(enforce e id1)) so what should we do with id stuff?
+	--enforce (If exp stmt) = if (fst $ enforce exp e) == Bool_ then Undefined
+	--						  else error("Cannot unify expected type Bool_ with " ++ show(enforce exp e) ++"!");
+	--enforce (IfElse exp stmt1 stmt2) = if (fst $ enforce exp e) == Bool_ then Undefined
+	--						  else error("Cannot unify expected type Bool_ with " ++ show(enforce exp e) ++"!");
+	--enforce (While exp stmt) = if (fst $ enforce exp e) == Bool_ then Undefined
+	--						  else error("Cannot unify expected type Bool_ with " ++ show(enforce exp e) ++"!");
+	enforce (Seq stmt) = yield Undefined
+	enforce (FunCall_ funCall) = yield Undefined
+	enforce (Return exp) = enforce exp -- TODO: check if it matches with the function specification
