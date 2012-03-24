f863f5d086aeab8fafe47addf4baec8724755b85
sebi <sebi>
2012-03-24 21:45:58 +0100

    Trying to blend in the environment but apparently with some errors

--- Typing.hs
+++ Typing.hs
@@ -3,20 +3,21 @@ import Data.List
 
 -- Environment should keep track of the current Prog and all the function names,
 -- it should also keep track of the function the checker is currently in.
-type Env = (Int, [(Type, Type)]) 
+type Env = (Int, [(Type, Type)],[(Type,Id)]) 
 type TypeChecker =  Env -> (Type, Env)
 
 -- Generate a new type
+
 freshVar :: TypeChecker
-freshVar (x, m) = (Generic_ ("_a" ++ show (x + 1)), (x + 1, m))
+freshVar = \(i,m,n)->(Generic_ ("_a" ++ show (i + 1)), (i + 1, m, n))
 
 -- Add map to environment and check if it is compatible
-addMap :: Type -> Type -> Env -> Env
-addMap a@(Generic_ _) b e@(i, m) = case find ((==a).fst) m of
+addMap :: Type -> Type -> TypeChecker
+addMap a@(Generic_ _) b = \e@(i, m, n) -> case find ((==a).fst) m of -- this means if it is already in the environment
 	Just (_, c@(Generic_ _)) -> addMap b c e -- Add a map from b to c
-	Just (_, c) -> if (c == b) then e else error $ "Cannot unify types: " ++ show b ++ "," ++ show c
-	Nothing -> (i, (a, b) : m)
-addMap _ _ e = e
+	Just (_, c) -> if (c == b) then yield Undefined e else error $ "Cannot unify types: " ++ show b ++ "," ++ show c
+	Nothing -> yield Undefined (i, (a, b) : m, n)
+addMap _ _ = yield Undefined
 
 -- check if the type a is in the right part
 -- useful, because such substitutions are not allowed
@@ -25,60 +26,73 @@ isIncluded k (Tuple_ a b) = isIncluded k a || isIncluded k b
 isIncluded k (List_ a) = isIncluded k a
 isIncluded a b = a == b
 
+infixl 5 >->
+(>->) :: (Env -> (a, Env)) -> (a -> b) -> (Env -> (b, Env))
+(>->) p k cs =
+  case p cs of
+    (a, cs') -> (k a, cs')
+	
 infixl 6 +=+
 (+=+) :: TypeChecker -> TypeChecker -> Env -> ((Type, Type), Env)
 (+=+) a b e = case a e of
 	(t1, e1) -> 
 		case b e1 of
 			(t2, e2) -> ((t1,t2),e2)
+			
+-- infixl 1 >>-
+-- (>>-) :: TypeChecker -> (Type -> TypeChecker) -> TypeChecker
+-- x >>- y = \env -> \(t,env) -> ((y t) env) (x env)
 
-infixl 5 >->
-(>->) :: (Env -> (a, Env)) -> (a -> b) -> (Env -> (b, Env))
-(>->) p k cs =
-  case p cs of
-    (a, cs') -> (k a, cs')
-
-yield :: a -> (Env -> (a, Env))
+-- infixl 1 >>|
+-- (>>|) :: TypeChecker -> TypeChecker -> TypeChecker 
+-- x >>| y = x >>- \_-> y
+			
+yield :: Type -> TypeChecker
 yield x = \e -> (x,e)
 
 class Substitute a where
-	substitute :: Type -> Type -> a -> a
+	substitute :: (Type, Type) -> a -> a
 	typeScraper :: a -> [Type]
-	uniqueName :: a -> Env -> ([(Type, Type)], Env)
-	makeUnique :: a -> [(Type, Type)] -> a -- TODO: add env
-
+	makeUnique :: a -> [(Type,Type)] -> a
+	uniqueName :: a -> TypeChecker
+	substInEnv :: a -> TypeChecker
+	
 instance Substitute Decl where
-	substitute a b (VarDecl v) = VarDecl (substitute a b v)
-	substitute a b (FunDecl f) = FunDecl (substitute a b f)
+	substitute (a,b) (VarDecl v)  = VarDecl (substitute (a,b) v)
+	substitute (a,b) (FunDecl f) =  FunDecl (substitute (a,b) f)
 
 -- BUG: first occurence is not replaced
 instance Substitute FunDecl where
-	substitute a b (FD ret name args vars stmts) = FD (substitute a b ret) name (newargs args) (map (substitute a b) vars) stmts
+	substitute (a,b) (FD ret name args vars stmts) = FD (substitute (a,b) ret) name (newargs args) (map (substitute (a,b)) vars) stmts
 		where
 		newargs = map (\(x,i) -> if (x == a) then (b,i) else (x, i))
 	typeScraper (FD ret name args vars stmts) = typeScraper ret ++ (concatMap (typeScraper . fst) args) ++ (concatMap typeScraper vars)
-
-	uniqueName f (i, e) = (zip vars [Generic_ ("_a" ++ show (x + 1)) | x <- [i..]], (length vars + i, e))
+	uniqueName f = \(i,m,n) -> (Undefined,(length vars + i,(zip vars [Generic_ ("_a" ++ show (x + 1)) | x <- [i..]]) ++ m, n))
 		where
 		vars = nub $ typeScraper f
 
-	makeUnique f [(a, b)] =  substitute a b f
+--	substInEnv f = \(i,m,n) -> \k@(FD ret name args vars stmts)->(Undefined,(i,m,[(getType k,name)])) (makeUnique f m)
+		
+	makeUnique f [(a, b)] =  substitute (a, b) f
 	makeUnique f l = makeUnique f (tail l)
-
+   
 instance Substitute VarDecl where
-	substitute a b (VD t name e) = VD (substitute a b t) name e
+	substitute (a,b) (VD t name e) = VD (substitute (a,b) t) name e
 	typeScraper (VD t name e) = typeScraper t
-	uniqueName f (i, e) = (zip (typeScraper f) [(Generic_ ("_a" ++ show (i + 1)))], (i + 1, e))
+	uniqueName f  = \(i,m,n) -> (Undefined,(i + 1,(zip (typeScraper f) [(Generic_ ("_a" ++ show (i + 1)))]) ++ m, n))
+	
+--  substInEnv v = \(i,m,n) -> \k@(VD t name e)->(Undefined,(i,m,[(t,name)])) (makeUnique v m)
+	
+	makeUnique v [(a, b)] =  substitute (a, b) v
+	makeUnique v l = makeUnique v (tail l)
 
 instance Substitute Type where
-	substitute a b c = if (a == c) then b else c
+	substitute (a,b) c = if (a == c) then b else c -- here I add the rulles
 	typeScraper k@(Generic_ t) = [k]
 	typeScraper _ = []
 
-
-fullSubstitute :: Type -> Type -> Prog -> Prog
-fullSubstitute a b c = map (substitute a b) c
-
+fullSubstitute :: (Type,Type) -> Prog -> Prog
+fullSubstitute (a,b) c = map (substitute (a,b)) c
 
 class TypeCheck a where
 	-- what should this function return? If it only modifies the environment, it is maybe ok as well.
@@ -98,12 +112,16 @@ instance TypeCheck Exp where
 	enforce (Op1_ o a) = \e-> if fst ((enforce a)e) == Bool_ then (yield Bool_) e else error $ "Expected bool, but got " ++ show (fst ((enforce a)e))
 	enforce EmptyList = freshVar >-> (\x -> List_ x)
 	enforce (Tuple a b) = enforce a +=+ enforce b >-> (\(c, d) -> Tuple_ c d)
---	enforce (Generic_ v) = yield v
---	enforce (FunCall (f, _)) = getReturnType f
+--	enforce (Id name) = case getType name of Int  = yield Int_
+--											Bool  = yield Bool_
+						
+
+--	enforce (FunCall (f, _)) = 
 
 instance TypeCheck Stmt where	
-	
-	--	enforce (Assign ids exp) = \e-> if fst ((enforce ids)e) == fst ((enforce exp)e) && fst ((enforce ids)e) == Int_ then (yield Int_) e else error $ "Expected type int " ++ "," ++ " int but got " ++ show (fst ((enforce ids)e)) ++ "," ++ show (fst ((enforce exp)e))
+--	TO DO for lists and tuples
+--	enforce (Assign ids exp) = \e-> if fst ((enforce (getType ids))e) == fst ((enforce exp)e) && fst ((enforce (getType ids))e) == Int_ then (yield Int_) e else error $ "Expected type int " ++ "," ++ " int but got " ++ show (fst ((enforce ids)e)) ++ "," ++ show (fst ((enforce exp)e))
+--	enforce (Assign ids exp) = \e-> if fst ((enforce (getType ids))e) == fst ((enforce exp)e) && fst ((enforce (getType ids))e) == Int_ then (yield Bool_) e else error $ "Expected type bool " ++ "," ++ " int but got " ++ show (fst ((enforce ids)e)) ++ "," ++ show (fst ((enforce exp)e))
 	enforce (If exp stmt) =  \e-> if ((fst ((enforce exp)e)) == Bool_) then (yield Undefined)e else error("Cannot unify expected type Bool_ with " ++ show((enforce exp)e) ++"!")
 	enforce (IfElse exp stmt1 stmt2) = \e->if ((fst ((enforce exp) e)) == Bool_) then (yield Undefined)e else error("Cannot unify expected type Bool_ with " ++ show((enforce exp) e) ++"!")
 	enforce (While exp stmt) = \e->if ((fst ((enforce exp)e)) == Bool_) then (yield Undefined)e else error("Cannot unify expected type Bool_ with " ++ show((enforce exp)e) ++"!")
