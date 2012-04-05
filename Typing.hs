c21fed0d677b53ec25e738101f30d3a0bb902996
Ben Ruijl <benruyl@gmail.com>
2012-04-05 15:25:17 +0200

    Implemented a Map to store all local function symbols

--- Typing.hs
+++ Typing.hs
@@ -3,16 +3,18 @@ module Typing where
 import AST
 import Data.List
 
+import qualified Data.Map as Map
+
 -- Environment should keep track of the current Prog and all the function names,
 -- it should also keep track of the function the checker is currently in.
 -- Env = (fresh var count, typemap, symbol table, current function id)
-data Scope = Global | Local Id | FunctionCall deriving Show
+data Scope = Global | Local Id | FunctionCall deriving (Show, Eq)
 type SymbolTable = ([(Type, Type)], [(Id, Type)])
-type Env = (Int, SymbolTable, SymbolTable, SymbolTable, Scope) 
+type Env = (Int, SymbolTable, Map.Map Id SymbolTable, SymbolTable, Scope) 
 type TypeChecker =  Env -> (Type, Env)
 
 showEnv :: Env -> String
-showEnv (i,m,l,f,c) = "Fresh var count: " ++ show i ++ "\nGlobal symbol table:" ++ show m ++ "\nLocal symbol table: " ++ show l ++ "\nFunction call symbol table: " ++ show f ++ "\nCurrent scope: " ++ show c
+showEnv (i,m,l,f,c) = "Fresh var count: " ++ show i ++ "\nGlobal symbol table:" ++ show m ++ "\nLocal symbol table:\n" ++ Map.showTree l ++ "\nFunction call symbol table: " ++ show f ++ "\nCurrent scope: " ++ show c
 
 -- check if the type a is included in type b
 isIncluded :: Type -> Type -> Bool
@@ -28,7 +30,8 @@ unify a (Generic_ b) = unify (Generic_ b) a
 unify a b =  if a == b then id else error $ "Cannot unify types: " ++ show a ++ " and " ++ show b
 
 -- Gets the function type the checker is currently in
-getLocalFunc :: Env -> (Type, Env)
+-- TODO: rename
+getLocalFunc :: Env -> Type
 getLocalFunc e@(i,m,n,l,Local id) = getSymbol id e
 getLocalFunc _ =  error $ "Not in function"
 
@@ -39,17 +42,12 @@ getScope = \e@(i,m,l,f,c) -> c
 setScope :: Scope -> Env -> Env
 setScope s = \e@(i,m,l,f,c) -> (i, m, l, f, s)
 
--- Clear the local table
--- TODO: store in another map
-clearLoc :: Env -> Env
-clearLoc = \e@(i,m,l,f,c) -> (i,m,([],[]),f,c)
-
 -- Add variable or function to the global symbol table
 addGlob :: SymbolTable -> Env -> Env
 addGlob (t, d) = \(i,(ot, od),l,f,c) -> (i,(t ++ ot, d ++ od),l,f,c)
 
 addLoc :: SymbolTable -> Env -> Env
-addLoc (t, d) = \(i,g,(ot, od),f,c) -> (i,g,(t ++ ot, d ++ od),f,c)
+addLoc (t, d) = \(i,g,l,f, Local id) -> (i,g, Map.insertWith (\(a,b) (ot, od) -> (a++ot, b++od)) id (t,d) l, f, Local id)
 
 -- Adds symbols in the function scope
 addFunc :: SymbolTable -> Env -> Env
@@ -65,24 +63,28 @@ addSymbolType s e@(i, m, l, f, Global) = addGlob ([s], []) e
 addSymbolType s e@(i, m, l, f, Local id) = addLoc ([s], []) e
 addSymbolType s e@(i, m, l, f, FunctionCall) = addFunc ([s], []) e
 
-getSymbol :: Id -> Env -> (Type, Env)
+getSymbol :: Id -> Env -> Type
 getSymbol id e@(i, (_, s), l, f, Global) = case find (\(i,t) -> i == id) s of
-	Just (i, t) -> (t, e)
+	Just (i, t) -> t
 	Nothing -> error $ "Undefined variable or function '" ++ id ++ "', " ++ showEnv e
-getSymbol id e@(i, m, l@(_, s), f, _) = case find (\(i,t) -> i == id) s of
-	Just (i, t) -> (t, e)
-	Nothing -> fst (getSymbol id (i, m, l, f, Global), e) -- search globally
+getSymbol id e@(i, m, l, f, Local fid) = case Map.lookup fid l of 
+	Just (_, s) ->  case find (\(i,t) -> i == id) s of
+		Just (i, t) -> t
+		Nothing -> getSymbol id (i, m, l, f, Global) -- search globally
+	Nothing -> error $ "Unknown function " ++ fid -- TODO: this shouldn't happen!
 
 getSymbolType t e@(_, (s, _), _, _, c) = getSymbolType' t c e
 
 getSymbolType' :: Type -> Scope -> Env-> Maybe (Type, Type) -- TODO: should it give Env back?
 getSymbolType' id Global e@(_, (s, _), _, _, _) = find (\(i,t) -> i == id) s
-getSymbolType' id (Local _) e@(_, _, (s, _), _, _) = case find (\(i,t) -> i == id) s of
+getSymbolType' id (Local fid) e@(_, _, l, _, _) = case Map.lookup fid l of 
+	Just (s,_) -> case find (\(i,t) -> i == id) s of
+		Just (i, t) -> Just (i, t)
+		Nothing -> getSymbolType' id Global e
+	Nothing -> error $ "Unknown function " ++ fid -- TODO: this shouldn't happen!
+getSymbolType' id FunctionCall e@(_, _, _, (s, _), _) = case find (\(i,t) -> i == id) s of
 	Just (i, t) -> Just (i, t)
-	Nothing -> getSymbolType' id Global e
-getSymbolType' id FunctionCall e@(_, _, (s, _), _, _) = case find (\(i,t) -> i == id) s of
-	Just (i, t) -> Just (i, t)
-	Nothing -> getSymbolType' id (Local "") e -- TODO: does it matter what the function is?
+	Nothing -> getSymbolType' id (Local "") e -- FIXME: it matter what the function is, this will fail!
 
 -- Adds a map from a generic to another type
 addMap :: Type -> Type -> Env -> Env
@@ -92,7 +94,7 @@ addMap a@(Generic_ _) b = \e -> case getSymbolType a e of
 addMap a b = if a == b then id else error $ "Cannot unify types: " ++ show a ++ " and " ++ show b
 
 cleanEnv :: Env
-cleanEnv = (0, ([],[]), ([],[]), ([],[]), Global)
+cleanEnv = (0, ([],[]), Map.empty, ([],[]), Global)
 
 -- Builds a global environment from a program
 -- TODO: Should this be done?
@@ -100,7 +102,7 @@ buildEnv :: Prog -> Env
 buildEnv p = foldl (\x y -> case y of 
 		 (VarDecl (VD _ name _)) -> addSymbol (name, getType y x) x
 		 (FunDecl (FD _ name _ _ _)) -> addSymbol (name, getType y x) x)
-		(defaultFunctions (0, ([],[]), ([],[]), ([],[]), Global)) p
+		(defaultFunctions (0, ([],[]), Map.empty, ([],[]), Global)) p
 
 -- Adds the default functions
 -- Assumes the Scope is set to Global
@@ -139,7 +141,7 @@ instance TypeCheck Decl where
 instance TypeCheck VarDecl where
 	-- VarDecl has its own scope
 	-- TODO: not the case for varDecls in functions
-	enforce (VD t name exp) = \e -> (unify t (getType exp e) . addSymbol (name, t) . (setScope (Local name)) .clearLoc) e
+	enforce (VD t name exp) = \e -> (unify t (getType exp e) . addSymbol (name, t) . (setScope (Local name))) e
 	getType (VD t name exp) = const t
 
 instance TypeCheck FunDecl where
@@ -154,13 +156,13 @@ instance TypeCheck Exp where
 	getType (Int _) = const Int_
 	getType (Bool _) = const Bool_
 	getType (Tuple a b) = \e -> Tuple_ (getType a e) (getType b e)
-	getType (Id name) = fst . getSymbol name
+	getType (Id name) = getSymbol name
 	getType EmptyList = const (List_ (Generic_ "_list")) -- FIXME: what to do here?
 	-- FIXME: find all cases
 	getType (ExpOp_ AppCons a b) = getType b
 	getType (ExpOp_ o a b) = if elem o [Equals, LessEq, MoreEq, NotEq, Less, More] then const Bool_ else const Int_
 	getType (Op1_ o a) = const Int_ -- FIXME: sometimes it's bool
-	getType (FunCall (name, args)) = \e -> case fst (getSymbol name e) of -- FIXME: correct, different scope?
+	getType (FunCall (name, args)) = \e -> case getSymbol name e of -- FIXME: correct, different scope?
 		Function v r -> r
 
 	enforce (Int _) = id
@@ -172,15 +174,15 @@ instance TypeCheck Exp where
 
 	enforce (Op1_ o a) = \e -> unify (getType a e) Int_ e
 	enforce (Tuple a b) = enforce a . enforce b
-	enforce (Id name) = snd . getSymbol name 
+	enforce (Id name) = seq (getSymbol name)  -- check if variable is defined
 	-- TODO: check if number of arguments is the same
-	enforce (FunCall (name, args)) = \e -> case fst (getSymbol name e) of
+	enforce (FunCall (name, args)) = \e -> case getSymbol name e of
 		Function v r -> foldl (\x (a,b)-> unify a b x) e (zip v (map (\x -> getType x e) args))
 
 instance TypeCheck Stmt where	
 	enforce (If cond stmt) = (\e -> unify (getType cond e) Bool_ e) . enforce stmt
 	enforce (IfElse cond stmt1 stmt2) = (\e -> unify (getType cond e) Bool_ e) . enforce stmt1 . enforce stmt2
-	enforce (Assign id exp) = \e -> unify (fst (getSymbol id e)) (getType exp e) e
+	enforce (Assign id exp) = \e -> unify (getSymbol id e) (getType exp e) e
 	enforce (While cond stmt) = (\e -> unify (getType cond e) Bool_ e) . enforce stmt
 	enforce (Seq stmt) = \e -> foldl (\x y -> enforce y x) e stmt
 	enforce (FunCall_ funCall) = enforce (FunCall funCall) -- call the Exp enforcer
@@ -188,5 +190,5 @@ instance TypeCheck Stmt where
 		Just exp -> (\e -> unify (getType exp e) (getRet e) e)  . (enforce exp)
 		Nothing -> \e -> unify Void (getRet e) e
 		where getRet = \e -> case getLocalFunc e of 
-				(Function a r, _) -> r
+				(Function a r) -> r
 				_ -> error "Not in function" 
