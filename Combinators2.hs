9729646a2282238694d43c115a79d3b837e25b75
Ben Ruijl <benruyl@gmail.com>
2012-04-19 17:44:21 +0200

    Added convert for VarDecl, and added a monadic environment

---
+++ Combinators2.hs
@@ -0,0 +1,38 @@
+-- TODO: rename
+module Combinators2 where
+
+type M a b = b -> (a, b)
+
+yield :: a -> b -> (a, b)
+yield t e = (t, e)
+
+infixl 5 >->
+(>->) :: M a m -> (a -> b) -> M b m
+(>->) p k e =
+  case p e of
+    (a,e') -> (k a, e')
+
+-- mutate environment
+infixr 4 >-->
+(>-->) ::  (m -> m) -> M a m -> M a m
+(>-->) k p e =
+  case p e of
+    (a,e') -> (a, k e')
+    
+infixl 6 #
+(#) :: M a m -> M b m -> M (a,b) m
+(#) a b e =
+  case a e of
+    (c, e') ->
+      case b e' of
+        (q, e'') -> ((c,q), e'')
+
+infixl 6 !++!
+(!++!) :: M a m -> (a -> M b m) -> M b m
+(!++!) a b e =
+  case a e of
+    (c, e') -> b c e'
+
+iter :: (a -> M b m) -> [a] -> M [b] m
+iter f [] = yield []
+iter f (l:ls) = f l # iter f ls >-> (\(a,b) -> a:b)
--- IR.hs
+++ IR.hs
@@ -1,53 +1,74 @@
 module IR where
 
-import Prelude (String, Int, Bool(..), Maybe(..), error, const, lookup, ($))
+import Prelude hiding (seq, EQ)
+import Data.Map as Map
 import qualified AST
-import Typing
+import Typing (Env)
+import Combinators2
 
 type Label = String
-data BinOp = PLUS | MINUS | MUL | DIV | AND | OR | LSHIFT | RSHIFT | ARSHIFT | XOR
-data RelOp = EQ | NE| LT | GT| LE | GE | ULT | ULE | UGT | UGE
-data Exp = CONST Int | NAME String | TEMP Label | BINOP BinOp Exp Exp | MEM Exp | CALL Exp [Exp] | ESEQ Stm Exp
+data BinOp = PLUS | MINUS | MUL | DIV | AND | OR | LSHIFT | RSHIFT | ARSHIFT | XOR deriving Show
+data RelOp = EQ | NE| LT | GT| LE | GE | ULT | ULE | UGT | UGE deriving Show
+data Exp = CONST Int | NAME String | TEMP Label | BINOP BinOp Exp Exp | MEM Exp | CALL Exp [Exp] | ESEQ Stm Exp deriving Show
 -- TODO: what to do with the [Exp] in JUMP?
-data Stm = MOVE Exp Exp | EXP Exp | JUMP Exp [Exp] | CJUMP RelOp Exp Exp Label Label | SEQ Stm Stm | LABEL String
+data Stm = MOVE Exp Exp | EXP Exp | JUMP Exp [Exp] | CJUMP RelOp Exp Exp Label Label | SEQ Stm Stm | LABEL String deriving Show
 data IR = Ex Exp | Nx Stm | Cx (Label -> Label -> Stm)
 
-unEx :: IR -> Exp
-unNx :: IR -> Stm
-unCx :: IR -> (Label -> Label -> Stm)
+instance Show IR where
+	show (Ex e) = show e
+	show (Nx s) = show s
+	show (Cx f) = error "FAIL" -- FIXME: cannot show function 
+
+data Frame = Frame {curPos :: Int, varMap :: Map Label Int, wordSize :: Int} deriving Show
+data Reg = Reg { labelCount :: Int, regCount :: Int, curFrame :: Frame} deriving Show
+type F a = M a Reg
+
+newLabel :: F Label
+newLabel r@(Reg {labelCount=l}) = ("l" ++ show l, r{labelCount=l+1})
+
+newReg :: F Label
+newReg r@(Reg {regCount=l}) = ("l" ++ show l, r{regCount=l+1})
+
+addVarToFrame :: Label -> F Int
+addVarToFrame l r@(Reg {curFrame=f}) = case f of
+	(Frame {curPos=p,varMap=m} ) -> (p, r{curFrame=f{curPos=p+1, varMap= Map.insert l p m}})
 
 -- make a sequence from list
 seq :: [Stm] -> Stm
 seq [x] = x
 seq (x:xs) = SEQ x (seq xs)
 
--- TODO: make labels unique
 -- TODO: add special case for CONST 1, CONST 0 for Cx?
---unCx (Nx s) should not happen
-unEx (Ex e) = e
-unEx (Nx s) = ESEQ s (CONST 0)
-unEx (Cx c) = ESEQ (seq [(MOVE (TEMP "r") (CONST 1)), (c "t" "f"), (LABEL "f"), (MOVE (TEMP "r") (CONST 1)), (LABEL "t")]) (TEMP "r")
-unNx (Ex e) = EXP e
-unNx (Nx s) = s
-unNx (Cx c) = SEQ (c "a" "a") (LABEL "a")
-unCx (Ex e) = CJUMP EQ e (CONST 1)
-unCx (Cx c) = c
+unEx :: IR -> F Exp
+unEx (Ex e) = yield e
+unEx (Nx s) = yield $ ESEQ s (CONST 0)
+unEx (Cx c) = newReg # newLabel # newLabel >-> \((r,t),f) -> ESEQ (seq [(MOVE (TEMP r) (CONST 1)), (c t f), (LABEL f), (MOVE (TEMP r) (CONST 1)), (LABEL t)]) (TEMP r)
+
+unNx :: IR -> F Stm
+unNx (Ex e) = yield $ EXP e
+unNx (Nx s) = yield s
+unNx (Cx c) = newLabel >-> \a -> SEQ (c a a) (LABEL a)
+
+unCx :: IR -> F (Label -> Label -> Stm)
+unCx (Nx s) = error $ "Cannot convert Nx to Cx"
+unCx (Ex e) = yield $ CJUMP EQ e (CONST 1)
+unCx (Cx c) = yield c
 
 class Convert a where
-	convert :: a -> Env -> IR
+	convert :: a -> F IR
 
 instance Convert AST.Exp where
-	convert (AST.Int a)  =const $ Ex $ CONST a
-	convert (AST.Bool False) = const $ Ex $ CONST 0
-	convert (AST.Bool True) = const $ Ex $ CONST 1
+	convert (AST.Int a)  = yield $ Ex $ CONST a
+	convert (AST.Bool False) = yield $ Ex $ CONST 0
+	convert (AST.Bool True) = yield $ Ex $ CONST 1
+	convert (AST.Op1_ AST.UnitaryMinus a) = convert a !++! unEx >-> \k -> Ex $ BINOP MINUS (CONST 0) k
+	convert (AST.Op1_ AST.Negate a) = convert a !++! unEx >-> \k -> Ex $ BINOP XOR (CONST 1) k
 	{-convert EmptyList =
 	convert (ExpOp_ AppCons a EmptyList) = 
 	convert (ExpOp_ AppCons a b) =
 	convert (ExpOp_ o a b) = 
-	convert (Op1_ UnitaryMinus a) =
-	convert (Op1_ Negate a) =
-	convert (Tuple a b) = 
-	convert (Id name)-}
+	convert (Tuple a b) = -}
+--	convert (Id name) = MEM (BINOP PLUS (TEMP "fp") (CONST k))
 
 -- TODO: add negate, and, or
 {-getCond (AST.Bool True) = (EQ, CONST 1, CONST 1)
@@ -59,20 +80,20 @@ getCond (AST.ExpOp_ o x y) = (getOp o, convert x, convert y)
 		Nothing -> error $ "Undefined operator"
 	m = [(AST.Equals, EQ), (AST.Less, LT), (AST.More, GT), (AST.LessEq, LE), (AST.MoreEq, GE), (AST.NotEq, NE)]-}
 
-convert2 ::  AST.Stmt -> Env -> IR
+instance Convert AST.Stmt where
 -- TODO: where to jump on false?
---convert2 (If cond stmt) = \((o, x, y) -> CJUMP o x y (convert stmt) ) (getCond cond)
---convert2 (IfElse cond stmt1 stmt2) = \((o, x, y) -> CJUMP o x y (convert stmt1) (convert stmt2)) (getCond cond)
+--	convert (If cond stmt) = \((o, x, y) -> CJUMP o x y (convert stmt) ) (getCond cond)
+--	convert (IfElse cond stmt1 stmt2) = \((o, x, y) -> CJUMP o x y (convert stmt1) (convert stmt2)) (getCond cond)
+	convert (AST.While cond stmt) = convert cond # convert stmt !++! uncurry parseWhile
+		where
+		parseWhile c s = newLabel # newLabel !++! \(b,d) -> (unCx c >-> \x -> (x b d, b, d)) # unNx s # newLabel  >-> \(((cc, b, d),ss),t) -> Nx (seq [LABEL t, cc, LABEL b, ss, JUMP (NAME t) [], LABEL d])
+--	convert (AST.Assign id exp) = \e -> Nx (MOVE (MEM (BINOP PLUS (TEMP "fp") (CONST k))) (unEx $ convert exp e))
+	convert (AST.Seq stmt) = (iter (\x -> convert x !++! unNx) stmt) >-> \x -> Nx (seq x)
+{-	convert (AST.FunCall_ funCall) =
+	convert (AST.Return a) = -}
 
--- TODO: unique labels
-convert2 (AST.While cond stmt) = \e -> parseWhile (convert cond e) (convert2 stmt e) 
-	where
-	parseWhile c s = Nx (seq [LABEL "test", unCx c "body" "done", LABEL "body", unNx s, JUMP (NAME "test") [], LABEL "done"])
-{-convert2 (Assign id exp) =
-convert2 (While cond stmt) =
-convert2 (Seq stmt) =
-convert2 (FunCall_ funCall) =
-convert2 (Return a) = -}
+instance Convert AST.VarDecl where
+	convert (AST.VD t id exp) = addVarToFrame id # convert exp !++! \(k,e) -> unEx e >-> (\x -> (k,x)) >-> \(k,e) -> Nx $ MOVE (MEM (BINOP PLUS (TEMP "fp") (CONST k))) e
 
---convert3 :: AST.VarDecl -> Stm
---convert3 (VD t id exp) = 
+
+
