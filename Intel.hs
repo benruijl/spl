6bc24f9e2fd5bd634337e3d33a9a21d48e7c12fc
Ben Ruijl <benruyl@gmail.com>
2012-05-18 20:00:01 +0200

    Fixed function calls

--- Intel.hs
+++ Intel.hs
@@ -15,44 +15,44 @@ instance Assemble Exp where
 	assemble (MEM (BINOP PLUS (TEMP "_glob") (CONST x))) = ["push word[esi + " ++ show (4*x) ++ "]"]  
 	assemble (MEM (CONST a)) = ["push word[ebp +" ++ show (4* a) ++ "]"]
 	-- TODO: fix modulo
-	assemble (BINOP o a b) = assemble a ++ ["pop eax"] ++ assemble b ++ ["pop ebx", op o ++ " eax ebx]"]
+	assemble (BINOP o a b) = assemble a ++ assemble b ++ ["pop eax", "pop ebx", op o ++ " eax, ebx", "push eax"]
 		where
 		op o = case lookup o conv of
 			Just c -> c 
 		conv = [(PLUS, "add"), (MINUS, "sub"), (AND, "and"), (OR, "or"), (MUL, "mul"), (DIV, "div"), (MOD, "mod"), (XOR, "xor")]
 		
-	assemble (CALL (TEMP "_alloc") args) = concatMap assemble args ++ ["push " ++ show (4 * length args), "call malloc", "sub esp, " ++ show (4 * length args)] -- FIXME: add or sub? ; Add the elements to the address
+	assemble (CALL (TEMP "_alloc") args) = concatMap assemble args ++ ["push " ++ show (4 * length args), "call malloc", "add esp, " ++ show (4 * length args)] -- FIXME: add or sub? ; Add the elements to the address
 	assemble (CALL (TEMP "head") args) = assemble (head args) ++ ["ldh 0"]
 	assemble (CALL (TEMP "tail") args) = assemble (head args) ++ ["ldh 1"]
 	assemble (CALL (TEMP "fst") args) = assemble (head args) ++ ["ldh 0"]
 	assemble (CALL (TEMP "snd") args) = assemble (head args) ++ ["ldh 1"]
 	assemble (CALL (TEMP "empty") args) = assemble (head args) ++ ["ldh 1", "ldc 0", "eq"]
 	-- FIXME: only prints single digit
-	assemble (CALL (TEMP "print") args) = assemble (head args) ++ ["pop eax", "add eax,30h", "push eax", "mov eax,4", "mov ecx,esp", "mov ebx,1", "mov edx,1", "int 0x80", "pop eax"]
-	assemble (CALL (TEMP id) args) = concat (map assemble args) ++ ["ldc " ++ id, "jsr", "ajs -" ++ show (length args), "ldr RR"]
+	assemble (CALL (TEMP "print") args) = assemble (head args) ++ ["pop eax", "add eax,30h", "push eax", "mov eax,4", "mov ecx,esp", "mov ebx,1", "mov edx,1", "int 0x80", "sub esp, 4"]
+	assemble (CALL (TEMP id) args) = concat (map assemble args) ++ ["call " ++ id, "add esp, " ++ show (4 * length args)] -- FIXME: what happens if there is no return value?
 	
 instance Assemble Stm where
 	assemble (MOVE (MEM (BINOP PLUS (TEMP "_glob") (CONST x))) e) = ["ldr 4"] ++ assemble e ++ ["sta " ++ show x]
-	assemble (MOVE (MEM (TEMP "_res")) e) = assemble e ++ ["str RR"]
+	assemble (MOVE (MEM (TEMP "_res")) e) = assemble e ++ ["pop eax"]-- store in EAX
 	assemble (MOVE (MEM (CONST v)) b) = assemble b ++ ["pop eax", "mov [ebp + " ++ show (4*v) ++ "], eax"]
 	assemble (EXP e) = assemble e
 	assemble (LABEL l) = [l ++ ":"]
 	assemble (SEQ a b) = assemble a ++ assemble b
 	assemble (JUMP (NAME l) _) = ["jmp " ++ l]
-	assemble (CJUMP o a b t f) = assemble b ++ assemble a ++ ["pop eax", "cmp eax,[esp]", "sub esp, 4", op o ++ f, "jmp " ++ t]
+	assemble (CJUMP o a b t f) = assemble b ++ assemble a ++ ["pop eax", "cmp eax,[esp]", "add esp, 4", op o ++ f, "jmp " ++ t]
 		where
 		op o = case lookup o rel of
 			Just c -> c 
 		rel = [(EQ, "je"), (LT, "jl"), (GT, "jg"), (LE, "jle"), (GE, "jge"), (NE, "jne")]
 
 instance Assemble Global where
-	assemble (Global {curVarPos=p,globVarMap=m,varBody=bm}) = ["ajs " ++ show p] ++ concat (map (\(i,e) -> assemble e ++ ["stl " ++ show i]) getList)
+	assemble (Global {curVarPos=p,globVarMap=m,varBody=bm}) = ["sub esp, " ++ show (4 *p)] ++ concat (map (\(i,e) -> assemble e ++ ["stl " ++ show i]) getList)
 		where
 		getList = Map.elems (Map.intersectionWith (\x y -> (x, y)) m bm)
 	
 -- FIXME add or sub?
 instance Assemble Frame where
-	assemble (Frame {curPos=c, IR.id=i, varMap=v, body=b}) = [i ++ ": "] ++ ["push ebp", "mov ebp,esp", "add esp, " ++ show (4*c)] ++ assemble b ++ ["mov esp,ebp"] ++ ["pop ebp"] ++ lastOp
+	assemble (Frame {curPos=c, IR.id=i, varMap=v, body=b}) = [i ++ ": "] ++ ["push ebp", "mov ebp,esp", "sub esp, " ++ show (4*c)] ++ assemble b ++ ["mov esp,ebp"] ++ ["pop ebp"] ++ lastOp
 		where
 		lastOp = if i == "main" then ["mov ebx,0", "mov eax,1", "int 0x80"] else ["ret"]
 	
