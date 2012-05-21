e4eb228d8953784116b1687f39839e5562e55952
Ben Ruijl <benruyl@gmail.com>
2012-05-21 16:55:01 +0200

    Now removes bogus values from void functions from stack

--- Intel.hs
+++ Intel.hs
@@ -28,15 +28,16 @@ instance Assemble Exp where
 	assemble (CALL (TEMP "fst") args) = assemble (head args) ++ ["pop eax", "push dword[eax]"]
 	assemble (CALL (TEMP "snd") args) = assemble (head args) ++ ["pop eax", "push dword[eax + 4]"]
 	assemble (CALL (TEMP "empty") args) = assemble (head args) ++ ["pop eax", "mov eax, dword[eax + 4]", "xor eax, 1", "push eax"]
-	assemble (CALL (TEMP "print") args) = assemble (head args) ++ ["push dword print_int", "call printf", "add esp, 8"]
-	assemble (CALL (TEMP "printChar") args) = assemble (head args) ++ ["push dword print_char", "call printf", "add esp, 8"]
-	assemble (CALL (TEMP id) args) = concat (map assemble args) ++ ["call " ++ id, "add esp, " ++ show (4 * length args), "push eax"] -- FIXME: what happens if there is no return value?
+	assemble (CALL (TEMP "print") args) = assemble (head args) ++ ["push dword print_int", "call printf", "add esp, 8", "push 0"]
+	assemble (CALL (TEMP "printChar") args) = assemble (head args) ++ ["push dword print_char", "call printf", "add esp, 8", "push 0"]
+	-- if the function returns nothing, the value on the stack will be removed by EXP
+	assemble (CALL (TEMP id) args) = concat (map assemble args) ++ ["call " ++ id, "add esp, " ++ show (4 * length args), "push eax"]
 	
 instance Assemble Stm where
 	assemble (MOVE (MEM (BINOP PLUS (TEMP "_glob") (CONST x))) e) = ["ldr 4"] ++ assemble e ++ ["sta " ++ show x]
 	assemble (MOVE (MEM (TEMP "_res")) e) = assemble e ++ ["pop eax"]-- store in EAX
 	assemble (MOVE (MEM (CONST v)) b) = assemble b ++ ["pop eax", "mov [ebp - " ++ show (4*v) ++ "], eax"]
-	assemble (EXP e) = assemble e
+	assemble (EXP e) = assemble e ++ ["pop eax"] -- remove result from expression from stack -- TODO: add "pop eax", because the result is not captured?"
 	assemble (LABEL l) = [l ++ ":"]
 	assemble (SEQ a b) = assemble a ++ assemble b
 	assemble (JUMP (NAME l) _) = ["jmp " ++ l]
@@ -55,10 +56,10 @@ instance Assemble Global where
 instance Assemble Frame where
 	assemble (Frame {curPos=c, IR.id=i, varMap=v, body=b}) = [i ++ ": "] ++ ["push ebp", "mov ebp,esp", "sub esp, " ++ show (4*c)] ++ assemble b ++ ["mov esp,ebp"] ++ ["pop ebp"] ++ lastOp
 		where
-		lastOp = if i == "main" then ["mov ebx,0", "mov eax,1", "int 0x80"] else ["ret"]
+		lastOp = if i == "main" then ["push 0", "call exit"] else ["ret"]
 	
 instance Assemble Reg where
-	assemble (Reg { frameList=f, globVars=g}) = ["extern malloc,printf", "segment .data", "print_int db \"%d\", 10, 0", "print_char db \"%c\", 0", "section .text", "global main", "mov esi,ebp"] ++ assemble g ++  ["jmp main"] ++ concat (map assemble (Map.elems f))
+	assemble (Reg { frameList=f, globVars=g}) = ["extern malloc,printf,exit", "segment .data", "print_int db \"%d\", 10, 0", "print_char db \"%c\", 0", "section .text", "global main", "mov esi,ebp"] ++ assemble g ++  ["jmp main"] ++ concat (map assemble (Map.elems f))
 
 instance Assemble IR where
 	assemble (Ex e) = assemble e
